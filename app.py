ad8cb6ed77507f1343682f66d64d559d10fe1be5
iampsrv <iampsrv@gmail.com>
2023-10-12 18:47:50 +0530

    second commit

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "testing jenkins with GITSCM polling"
+    return "Hello Geeks"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
